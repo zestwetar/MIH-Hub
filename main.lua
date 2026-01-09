267f7bcf1357472a9617fcc8459ff4911dcc38da
zestwetar <sshorts013@gmail.com>
2026-01-09 15:47:56 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,34 @@
-print("MIH Hub main.lua calisti")
+-- MIH Hub | Main
+repeat task.wait() until game:IsLoaded()
 
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "MIH Hub",
-    Text = "Loader basariyla calisti",
-    Duration = 5
+-- Rayfield UI
+local Rayfield = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/dawid-scripts/UI-Libraries/main/Rayfield.lua"
+))()
+
+-- Window
+local Window = Rayfield:CreateWindow({
+    Name = "MIH Hub",
+    LoadingTitle = "MIH Hub",
+    LoadingSubtitle = "Public Version",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "MIH Hub",
+        FileName = "MIH_Config"
+    }
+})
+
+-- Tab
+local MainTab = Window:CreateTab("Main", 4483362458)
+
+-- Button (TEST)
+MainTab:CreateButton({
+    Name = "Test Button",
+    Callback = function()
+        Rayfield:Notify({
+            Title = "MIH Hub",
+            Content = "Rayfield UI calisiyor!",
+            Duration = 5
+        })
+    end
 })
