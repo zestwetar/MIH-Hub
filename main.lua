ab9e293cfbe2d3bbb03c8aabe048bbb65eb9664f
zestwetar <sshorts013@gmail.com>
2026-01-09 15:36:53 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+print("MIH Hub main.lua calisti")
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "MIH Hub",
+    Text = "Loader basariyla calisti",
+    Duration = 5
+})
